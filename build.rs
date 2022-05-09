ba74a1b031339103cc0e3c965701adb587acb04e
Serhii Hadzhilov <serhii-external@symless.com>
2022-05-09 21:37:24 +0300

    Windows specific build

--- build.rs
+++ build.rs
@@ -1,5 +1,15 @@
 extern crate cc;
 
+#[cfg(windows)]
+fn main() {
+    println!("cargo:rustc-link-lib=iphlpapi");
+    cc::Build::new()
+        .cpp(false)
+        .file("cmdns/src/mdns.c")
+        .compile("libcmdns.a");
+}
+
+#[cfg(not(windows))]
 fn main() {
     cc::Build::new()
         .cpp(false)
